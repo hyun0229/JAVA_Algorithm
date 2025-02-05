35153e4c27939d5a12312c814799462effddbe7e
Hyunsuk <dhgustjr29@naver.com>
2025-02-05 16:17:53 +0900

    bj_1600 풀이

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 | 4963번 | [섬의 개수](https://www.acmicpc.net/problem/4963) | [bj_4963_섬의개수.java](src/baekjoon/bj_4963_섬의개수.java) | S2 |
 | 2206번 | [벽 부수고 이동하기](https://www.acmicpc.net/problem/2206) | [bj_2206_벅부수고이동.java](src/baekjoon/bj_2206_벅부수고이동.java) | G3 |
 | 2981번 | [검문](https://www.acmicpc.net/problem/2981) | [bj_2981_검문.java](src/baekjoon/bj_2981_검문.java) | G4 |
+| 1600번 | [말이 되고픈 원숭이](https://www.acmicpc.net/problem/1600) | [bj_1600_말이되고픈원숭이.java](src/baekjoon/bj_1600_말이되고픈원숭이.java) | G3 |
 
 ### 📌 프로그래머스 (Programmers)
 
--- src/SWEA/SWEA_1247_최적경로.java
+++ src/SWEA/SWEA_1247_최적경로.java
@@ -1,5 +1,55 @@
 package SWEA;
 
-public class SWEA_1247_최적경로 {
+import java.util.Scanner;
 
+public class SWEA_1247_최적경로 {
+    public static int ans = 0;
+    public static int L = 0;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            ans = 0;
+            int N = sc.nextInt();
+            L = sc.nextInt();
+            int[][] bugers = new int[N][2];
+            for (int i = 0; i < N; i++) {
+                bugers[i][0] = sc.nextInt();
+                bugers[i][1] = sc.nextInt();
+            }
+            recursive(bugers, 0, 0, new int[N][2] );
+            System.out.printf("#%d %d\n",test_case,ans);
+        }
+    }
+    static void recursive(int[][] arr,int idx, int depth, int[][] sel) {
+        //basis part
+        int kcal = 0;
+        int score = 0;
+        for (int i = 0; i < depth; i++) {
+            score += sel[i][0];
+            kcal+= sel[i][1];
+        }
+        if(kcal>L) {
+            return;
+        }
+        else {
+            ans = Math.max(ans, score);
+        }
+
+        if(depth == sel.length) {
+            return;
+        }
+        if(idx ==arr.length) {
+            return;
+        }
+
+        sel[depth][0] =arr[idx][0];
+        sel[depth][1] =arr[idx][1];
+        recursive(arr, idx+1, depth+1, sel);
+        recursive(arr, idx+1, depth, sel);
+
+    }
 }
