24a8cba5ec808a2ffbc9a2d0954cef43a915c444
hyun0229 <dhgustjr29@naver.com>
2025-02-14 17:07:44 +0900

    boj_인터프린터 풀이중

---
+++ src/SWEA/SWEA_14510_나무높이.java
@@ -0,0 +1,46 @@
+package SWEA;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SWEA_14510_나무높이 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T= sc.nextInt();
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            int day1 = 0;
+            int day2 = 0;
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            int bigtree= arr[N-1];
+            for (int i = 0; i < arr.length; i++) {
+                int num = bigtree - arr[i];
+                int a = (int)(num/3);
+                int b = num%3;
+                day1 +=a;
+                day2 +=a;
+                if (b==1) {
+                    day1+=1;
+                }
+                if (b==2) {
+                    day2+=1;
+                }
+            }
+            int ans =0;
+
+            if (day1>day2) {
+                int num = day1-day2;
+                ans = (day1 - (num/2))*2-1;
+            }
+            else{
+                int num = day2-day1;
+                ans = (day2 - (num/2))*2;
+            }
+            System.out.println(String.format("#%d %d",test_case,ans));
+        }
+    }
+}
--- src/baekjoon/boj_3954_인터프리터.java
+++ src/baekjoon/boj_3954_인터프리터.java
@@ -3,10 +3,15 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class boj_3954_인터프리터 {
+    static final long limt_roop =  50000000;
+    static final long MAX_VALUE = (long) Math.pow(2, 8);
     static int sm, sc, si;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,14 +21,85 @@ public class boj_3954_인터프리터 {
             sm = Integer.parseInt(st.nextToken());
             sc = Integer.parseInt(st.nextToken());
             si = Integer.parseInt(st.nextToken()); 
-            int[] memory = new int[sm];
+            long[] memory = new long[sm];
             char[] ip = new String(br.readLine()).toCharArray();
-            char[] chars = new char[si];
-            check(memory,ip,chars);
+            char[] chars = new String(br.readLine()).toCharArray();
+            check(memory,ip,chars); 
         }
     }
                 
-    private static void check(int[] memory, char[] ip, char[] chars) {
-        
+    private static void check(long[] memory, char[] ip, char[] chars) {
+        long time = 0;
+        int i = 0;
+        int index = 0;
+        int char_index = 0;
+        Stack<long[]> stackL = new Stack<>();
+        //Stack<long[]> stackR = new Stack<>();
+        while (i!=sc) {
+            switch (ip[i]) {
+                case '-':
+                    memory[index]--;
+                    if(memory[index]== -1)memory[index] = MAX_VALUE;
+                    i++;
+                    break;
+                case '+':
+                memory[index]++;
+                    if(memory[index]== MAX_VALUE+1)memory[index] = 0;
+                    i++;
+                    break;
+                case '<':
+                    index--;
+                    if(index == -1)index = sm-1;
+                    i++;
+                    break;
+                case '>':
+                    index++;
+                    if(index == sm)index = 0;
+                    i++;
+                    break;
+                case ',':
+                    if(char_index == si)memory[index] +=255;
+                    else memory[index] += chars[char_index++];
+                    if(memory[index]> MAX_VALUE)memory[index] = memory[index]%MAX_VALUE-1;
+                    i++;
+                    break;
+                case '[':
+                    if (memory[index] == 0){
+                        while (true){
+                            if (ip[i++] == ']')break;
+                        }
+                    }
+                    else{
+                        if (stackL.isEmpty() || stackL.peek()[0] != i) {
+                            stackL.add(new long[]{i,-1});
+                            i++;
+                        }
+                    }
+                    break;
+                case ']':
+                    if (memory[index] == 0) {
+                        i++;
+                        stackL.pop();
+                    }
+                    else if(stackL.peek()[1] !=-1 && limt_roop<= time - stackL.peek()[1]){
+                        System.out.println(String.format("Loops %d %d", stackL.peek()[0], i));
+                        return;
+                    }
+                    else{
+                        i = (int) stackL.peek()[0]+1;
+                        if(stackL.peek()[1]==-1){
+                            stackL.pop();
+                            stackL.add(new long[]{i-1,time});
+                        }
+                    }
+                    break;
+                
+                default:
+                    i++;
+                    break;
+            }
+            time++;
+        }
+        System.out.println("Terminates");
     }
 }
