075acaa15fa2022a612e7a278a9c5b45b1eb47a8
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-11 16:06:22 +0900

    [Gold V] Title: Z, Time: 64 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.java
@@ -0,0 +1,46 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int ans = recursive(N,r,c);
+        System.out.println(ans);
+
+    }
+    private static int recursive(int lv,int x, int y) {
+        int num = 0;
+        int r =x;
+        int c = y;
+        int N = (int) (Math.pow(2, lv)/2);
+        if (r<N&&c<N){
+            num = 0;
+        }
+        else if (r<N&&c>=N) {
+            num = N*N;
+            c-=N;
+        }
+        else if (r>=N&&c<N) {
+            num = N*N*2;
+            r-=N;
+        }
+        else{
+            num = N*N*3;
+            r-=N;
+            c-=N;
+        }
+
+
+        if(lv==1)return num;
+        
+        return num+recursive(lv-1,r,c);
+    }
+}
