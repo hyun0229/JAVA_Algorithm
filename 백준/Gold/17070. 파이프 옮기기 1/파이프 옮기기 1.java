16a77984f12345acf68f618f082c548f490f014c
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-11 16:03:26 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 164 ms, Memory: 13480 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int ans = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int map[][] = new int[N][N];
+        for (int i = 0; i < map.length; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < map.length; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        recursive(map,0,1,N,1);
+        System.out.println(ans);
+    }
+    private static void recursive(int[][] map, int i, int j,int N,int state) {
+        if (i == N-1 && j ==N-1) {
+            ans++;
+            return;
+        }
+        if (j+1<N && map[i][j+1]!=1 && state!= -1 ) {
+            recursive(map, i, j+1, N,1);
+        }
+        if(i+1< N &&map[i+1][j]!=1 && state !=1){
+            recursive(map, i+1, j, N,-1);
+        }
+        if (i+1< N &&  j+1<N && map[i+1][j]!=1 &&map[i][j+1]!=1  &&map[i+1][j+1]!=1 ) {
+            recursive(map, i+1, j+1, N,0);
+        }
+    }
+
+}
