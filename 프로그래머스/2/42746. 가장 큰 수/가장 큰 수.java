9ec9160bf65ed296d56787d81a38740363a39a59
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-11 16:13:02 +0900

    [level 2] Title: 가장 큰 수, Time: 520.02 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<node> pq = new PriorityQueue<>();
+        for(int i : numbers){
+            pq.add(new node(i));
+        }
+        if(pq.peek().num.charAt(0)=='0') return "0";
+        while(!pq.isEmpty()){
+            node n = pq.poll();
+            sb.append(n.num);
+        }
+        
+        return sb.toString();
+    }
+    static class node implements Comparable<node>{
+        public String num;
+        int size;
+        public node(int num){
+            this.num = Integer.toString(num);
+            this.size = this.num.length();
+        }
+        @Override
+        public int compareTo(node o){
+            int n1 = Integer.parseInt(this.num+o.num);
+            int n2 = Integer.parseInt(o.num+this.num);
+            return n2-n1;
+        }
+    }
+}
