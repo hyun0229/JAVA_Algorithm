978c8761e89e3545e3aa43176650d4f25c19c88c
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-11 16:13:35 +0900

    [level 3] Title: 이중우선순위큐, Time: 100.57 ms, Memory: 143 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,69 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        PriorityQueue<node> minpq = new PriorityQueue<>();
+        PriorityQueue<node> maxpq = new PriorityQueue<>(Collections.reverseOrder());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < operations.length; i++) {
+            String[] parts = operations[i].split(" ");
+            String order = parts[0];
+            String num = parts[1];
+            if (order.equals("I")) {
+                String key = i + "," + num;
+                node tmp = new node(Integer.parseInt(num), key);
+                minpq.add(tmp);
+                maxpq.add(tmp);
+            } else {
+                if (num.equals("-1")) {
+                    while (!minpq.isEmpty()) {
+                        node tmp = minpq.poll();
+                        if (!set.contains(tmp.key)) {
+                            set.add(tmp.key);
+                            break;
+                        }
+                    }
+                }
+                if (num.equals("1")) {
+                    while (!maxpq.isEmpty()) {
+                        node tmp = maxpq.poll();
+                        if (!set.contains(tmp.key)) {
+                            set.add(tmp.key);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        while (!maxpq.isEmpty()) {
+            node tmp = maxpq.poll();
+            if (!set.contains(tmp.key)) {
+                answer[0] = tmp.num;
+                break;
+            }
+        }
+        while (!minpq.isEmpty()) {
+            node tmp = minpq.poll();
+            if (!set.contains(tmp.key)) {
+                answer[1] = tmp.num;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    static class node implements Comparable<node> {
+        int num;
+        String key;
+
+        public node(int num, String key) {
+            this.num = num;
+            this.key = key;
+        }
+
+        @Override
+        public int compareTo(node o) {
+            return Integer.compare(this.num, o.num);
+        }
+    }
+}
