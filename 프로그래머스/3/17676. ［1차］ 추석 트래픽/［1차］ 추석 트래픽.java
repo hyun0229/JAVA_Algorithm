f561ed0133b31cb1b1b1bdc76bf36889bcc30274
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-11 16:12:41 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 52.37 ms, Memory: 95 MB -BaekjoonHub

---
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+        public int solution(String[] lines) {
+            int answer = 0;
+            List<TimePair> logs = new ArrayList<>();
+            for (String log : lines) {
+                logs.add(timeParser(log));
+            }
+            for (TimePair timePair : logs) {
+                long s = timePair.end;
+                long e = s + 999;
+                int cnt = 0;
+                for (TimePair chk : logs) {
+                    if (chk.start <= e && chk.end >= s) {
+                        cnt++;
+                    }
+                }
+                answer = Math.max(answer, cnt);
+            }
+            return answer;
+        }
+        public static TimePair timeParser(String log) {
+            String[] parts = log.split(" ");
+            String timeStr = parts[1];
+            String dStr = parts[2]; 
+
+            String[] hms = timeStr.split(":");
+            int hour = Integer.parseInt(hms[0]);
+            int minute = Integer.parseInt(hms[1]);
+
+            String[] sec = hms[2].split("\\.");
+            int second = Integer.parseInt(sec[0]);
+            int millis = Integer.parseInt(sec[1]);
+
+            long end = hour * 3600000L + minute * 60000L + second * 1000L + millis;
+
+            double t = Double.parseDouble(dStr.replace("s", ""));
+            long num = (long)(t * 1000);
+
+            long start = end - num + 1;
+
+            return new TimePair(start, end);
+        }
+        static class TimePair {
+            long start,end;
+            TimePair(long start, long end){
+                this.start = start;
+                this.end = end;
+            }
+        }
+    }
