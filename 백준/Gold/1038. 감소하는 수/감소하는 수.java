b0991e654c53c522d13b75196eb741b1b33bb564
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-11 16:09:01 +0900

    [Gold V] Title: 감소하는 수, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+    static int count = -1;
+    static long answer = -1;
+    static boolean flag;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        flag = false;
+        for (int i = 0; i < 10; i++) {
+            if (flag)
+                break;
+            recursive(10, i, 0);
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void recursive(int max, int lv, long ans) {
+        if (lv == -1) {
+            count++;
+            if (count == N) {
+                flag = true;
+                answer = ans;
+            }
+            return;
+        }
+        for (int i = 0; i < max; i++) {
+            if (flag)
+                break;
+            recursive(i, lv - 1, ans * 10 + i);
+        }
+    }
+}
