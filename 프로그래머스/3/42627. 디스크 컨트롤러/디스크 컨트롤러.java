48ca429a144204f40ea5f9dfc9a9bf4a897d1f64
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-11 16:13:30 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.48 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,64 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        PriorityQueue<WaitTask> wait = new PriorityQueue<>();
+        PriorityQueue<RunTask> run = new PriorityQueue<>();
+        for(int i = 0; i<jobs.length;  i++){
+            int start = jobs[i][0];
+            int time = jobs[i][1];
+            wait.add(new WaitTask(start,new RunTask(i,start,time)));
+        }
+        int now = 0;
+        int sum = 0;
+        run.add(wait.poll().runTask);
+        now = run.peek().start;
+        while(!wait.isEmpty()){
+            if(wait.peek().start<=now){
+                run.add(wait.poll().runTask);
+            }
+            else if(run.isEmpty()){
+                run.add(wait.poll().runTask);
+                now = run.peek().start;
+            }
+            else{
+                RunTask rt = run.poll();
+                now += rt.runtime;
+                sum += now - rt.start;
+            }
+        }
+        while(!run.isEmpty()){
+            RunTask rt = run.poll();
+            now += rt.runtime;
+            sum += now - rt.start;
+        }
+        return sum/jobs.length;
+    }
+    static class RunTask implements Comparable<RunTask>{
+        int number; int start; int runtime;
+        RunTask(int number, int start, int runtime){
+            this.number = number;
+            this.start = start;
+            this.runtime = runtime;
+        }
+        @Override
+        public int compareTo(RunTask o){
+            if(this.runtime != o.runtime)return Integer.compare(this.runtime,o.runtime);
+            if(this.start != o.start)return Integer.compare(this.start,o.start);
+            return Integer.compare(this.number,o.number);
+        }
+    }
+    static class WaitTask implements Comparable<WaitTask>{
+        int start;
+        RunTask runTask;
+        WaitTask(int start,RunTask runTask){
+            this.start = start;
+            this.runTask = runTask;
+        }
+        @Override
+        public int compareTo(WaitTask o){
+            return Integer.compare(this.start,o.start);
+        }
+    }
+    
+}
