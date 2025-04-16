b4037883041fbe3c673293473bdc9f6dbfd90a9e
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-16 16:38:42 +0900

    [level 2] Title: 큰 수 만들기, Time: 43.08 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -5,12 +5,17 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for(int i = 0;i<slen;i++){
             char tmp = number.charAt(i);
+            //k만큼 숫자를 빼야함 + 맨 앞지리 숫자가 제일 크면 큰수임
+            //그래서 맨 앞자리 숫자를 큰수를 유지하기위해 숫자가 들어면 비교해서 맨 뒷자리 부터 빼버림
+            //단 k가 0이 될 경우 멈춰!
             while(k!=0&&sb.length()!=0&&tmp-sb.charAt(sb.length() - 1)>0){
                 sb.deleteCharAt(sb.length() - 1);
                 k--;
             }
+            //앞에 나보다 작은 숫자는 다 제거했으니 내가 들어감
             sb.append(tmp);
         }
+        //k가 0이 아닌데 끝났네?? 그럼 맨 뒤에서부터 number의 길이 - k가 답의 길이일테니 뒤에서부터 제가함
         while(anslen<sb.length()){
             sb.deleteCharAt(sb.length() - 1);
         }
