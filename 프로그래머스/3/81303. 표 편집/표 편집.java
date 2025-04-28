3f07af1e4ead51956dd5621409fe923a7a7a11d7
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-04-28 15:26:04 +0900

    [level 3] Title: 표 편집, Time: 718.98 ms, Memory: 202 MB -BaekjoonHub

---
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -0,0 +1,68 @@
+import java.util.*;
+class Solution {
+    boolean[] map;
+    int now;
+    int last;
+    TreeSet<Integer> set;
+    public String solution(int n, int k, String[] cmd) {
+        String answer = "";
+        map = new boolean[n];
+        set = new TreeSet<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i = 0; i<n;i++){
+            map[i] = true;
+            set.add(i);
+        }
+        now = k;
+        last = 0;
+        for(String s : cmd){
+            String tmp[] = s.split(" ");
+            if(tmp[0].equals("D")){
+                int cnt = Integer.parseInt(tmp[1]);
+                downMove(cnt);
+            }
+            if(tmp[0].equals("U")){
+                int cnt = Integer.parseInt(tmp[1]);
+                upMove(cnt);    
+            }
+            if(tmp[0].equals("C")){
+                map[now]= false;
+                set.remove(now);
+                dq.add(now);
+                Integer next = set.higher(now);
+                if(next == null)next = set.lower(now);
+                now = next;
+            }
+            if(tmp[0].equals("Z")){
+                int recover = dq.pollLast();
+                map[recover] = true;
+                set.add(recover);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(boolean chk : map){
+            if(chk)sb.append('O');
+            else sb.append('X');
+        }
+        return sb.toString();
+    }
+    void upMove(int cnt) {
+        int tmp = now;
+        for (int i = 0; i < cnt; i++) {
+            Integer next = set.lower(tmp);
+            if (next == null) break; // 더 이상 갈 수 없으면 멈추기
+            tmp = next;
+        }
+        now = tmp;
+    }
+
+    void downMove(int cnt) {
+        int tmp = now;
+        for (int i = 0; i < cnt; i++) {
+            Integer next = set.higher(tmp);
+            if (next == null) break;
+            tmp = next;
+        }
+        now = tmp;
+    }
+}
