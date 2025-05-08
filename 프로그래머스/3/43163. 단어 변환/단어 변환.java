157d325ff8ded3e527b3dfd611b48a5d8724bed5
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2025-05-08 16:31:24 +0900

    [level 3] Title: 단어 변환, Time: 13.55 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,53 +1,42 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int solution(String begin, String target, String[] words) {
-        HashMap<String,Word> map = new HashMap<>();
-        map.put(begin, new Word());
-        for(String s : words){
-            Word tmp = new Word();
-            for (Map.Entry<String, Word> entry : map.entrySet()) {
-                int cnt = 0;
-                if(!check(entry.getKey(), s)) continue;
-                entry.getValue().arr.add(s);
-                tmp.arr.add(entry.getKey());
+        List<String> list = new ArrayList<>(Arrays.asList(words));
+        list.add(begin);
+
+        // 중간 패턴 맵: "*ot" → ["hot","dot", ...]
+        Map<String, List<String>> patternMap = new HashMap<>();
+        int L = begin.length();
+        for (String w : list) {
+            for (int i = 0; i < L; i++) {
+                String pat = w.substring(0, i) + '*' + w.substring(i + 1);
+                patternMap.computeIfAbsent(pat, k -> new ArrayList<>()).add(w);
             }
-            map.put(s,tmp);
         }
+
+        // BFS
         Queue<String> q = new ArrayDeque<>();
-        q.add(begin);
-        map.get(begin).vis = 0;
-        while(!q.isEmpty()){
-            String now = q.poll();
-            int num = map.get(now).vis;
-            ArrayList<String> arr = map.get(now).arr;
-            for(String s : arr){
-                Word tmp = map.get(s);
-                if(tmp.vis != -1)continue;
-                tmp.vis = num+1;
-                q.add(s);
-                if(s.equals(target)){
-                    return num +1;
+        Map<String, Integer> dist = new HashMap<>();
+        q.add(begin); dist.put(begin, 0);
+
+        while (!q.isEmpty()) {
+            String cur = q.poll();
+            int d = dist.get(cur);
+            // 한 글자씩 바꿔볼 패턴 생성
+            for (int i = 0; i < L; i++) {
+                String pat = cur.substring(0, i) + '*' + cur.substring(i + 1);
+                List<String> neighbors = patternMap.getOrDefault(pat, Collections.emptyList());
+                for (String nxt : neighbors) {
+                    if (!dist.containsKey(nxt)) {
+                        dist.put(nxt, d + 1);
+                        if (nxt.equals(target)) return d + 1;
+                        q.add(nxt);
+                    }
                 }
             }
         }
+
         return 0;
     }
-    public class Word{
-        int vis;
-        ArrayList<String> arr;
-        public Word(){
-            vis = -1;
-            arr = new ArrayList<>();
-        }
-    }
-    public boolean check(String o1, String o2){
-        int cnt = 0;
-        for(int i = 0; i < o1.length() ; i++){
-            if(o1.charAt(i) != o2.charAt(i)) cnt++;
-            if(cnt==2) return false;
-        }
-        return true;
-    }
 }
-
