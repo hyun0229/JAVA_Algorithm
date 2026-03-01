c3bbbde563204d6323de2317c796952ec114426e
hyunsuk <98635207+hyun0229@users.noreply.github.com>
2026-03-01 15:57:41 +0900

    [Gold III] Title: 직사각형 만들기, Time: 868 ms, Memory: 245764 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/30091. 직사각형 만들기/직사각형 만들기.py
+++ 백준/Gold/30091. 직사각형 만들기/직사각형 만들기.py
@@ -1,27 +1,33 @@
-N = int(input())
-ls = list(map(int, input().split()))
-hmap = {}
-for i in range(N):
-    for j in range(i + 1, N):
-        l = ls[i]+ ls[j]
-        if l not in hmap:
-            hmap[l] = {
-                "number" : set(),
-                "sum": 0
-            }
-            hmap[l]["number"].add(i)
-            hmap[l]["number"].add(j)
-            hmap[l]["sum"]+=ls[i]*ls[j]
-        else:
-            if i in hmap[l]["number"] or j in hmap[l]["number"]:
-                continue
-            else:
-                hmap[l]["number"].add(i)
-                hmap[l]["number"].add(j)
-                hmap[l]["sum"]+=ls[i]*ls[j]
-ans = 0
-for k in hmap:
-    if ans < hmap[k]["sum"]:
-        ans = hmap[k]["sum"]
+import sys
+from collections import Counter, defaultdict
 
-print(ans)
+
+input = sys.stdin.readline
+N = int(input().strip())
+L = list(map(int, input().split()))
+
+cnt = Counter(L)              # 길이별 개수
+vals = sorted(cnt.keys())     # 서로 다른 길이들
+sum_area = defaultdict(int)   # S(=a+b) -> 넓이합
+
+K = len(vals)
+for i in range(K):
+    a = vals[i]
+    ca = cnt[a]
+
+    # a == b 인 경우: 같은 길이끼리 묶기
+    pairs_same = ca // 2
+    if pairs_same:
+        S = a + a
+        sum_area[S] += pairs_same * a * a
+
+    # a < b 인 경우
+    for j in range(i + 1, K):
+        b = vals[j]
+        pairs = ca if ca < cnt[b] else cnt[b]   # min(cnt[a], cnt[b])
+        if pairs:
+            S = a + b
+            sum_area[S] += pairs * a * b
+
+print(max(sum_area.values(), default=0))
+
